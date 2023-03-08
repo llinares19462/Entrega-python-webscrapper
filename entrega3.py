72d894a797153f5916f822009c888de35db5256f
llinares19462 <79168355+llinares19462@users.noreply.github.com>
2023-03-08 18:13:18 +0100

    Archivos python practica web scrapping

---
+++ entrega3.py
@@ -0,0 +1,44 @@
+import requests # parte del webscrapping
+from bs4 import BeautifulSoup # hace el web scraping
+import os # para guardar el resutlado en el disco
+
+# URL de la búsqueda de imágenes
+# url = 'https://unsplash.com/s/photos/ciberseguridad'
+url = 'https://en.wikipedia.org/wiki/PC1512'
+
+# Realizar una solicitud GET a la URL y obtener el contenido HTML de la página
+response = requests.get(url)
+html = response.content
+# print(html)
+# exit()
+
+# Analizar el HTML utilizando BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser') # se parsea el codigo para poder hacer mejor las busquedas
+
+# Encontrar todas las etiquetas 'img' en el HTML
+a_tags = soup.find_all('a') 
+# print(a_tags)
+# exit()
+
+# Crear un directorio para guardar las imágenes descargadas
+# if not os.path.exists('parrafos_amstrad'):
+#    os.makedirs('parrafos_amstrad')
+
+# Iterar sobre todas las etiquetas 'img' encontradas y descargar las imágenes
+with open('enlaces_a.txt', 'w') as f:
+ for a_tag in a_tags:
+  try:
+   href_attr = a_tag['href']
+   f.write(str(href_attr)+"\n")
+  except:
+   print("Este elemento no tiene href")
+#    print(a_tag)
+   
+#    img_url = img_tag['src']
+#    if 'images.unsplash.com/photo' in img_url:
+#        response = requests.get(img_url)
+#        filename = img_url.split('/')[-1]
+#        with open(os.path.join('parrafos_amstrad', filename), 'wb') as f:
+#            f.write(response.content)
+#            print(f"Imagen descargada: {filename}")
+
